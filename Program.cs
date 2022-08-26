7bd6ca7aae7dc951da3156d13d3fc50e31a62c5d
devkandjandja <kandjandja94@gmail.com>
2022-08-26 01:57:25 +0100

    Calculadora, metendo mão na massa para carreira

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float resultado = v1 + v2;
+        Console.WriteLine($"Resultado da soma é: {resultado}");
+    }
+}
